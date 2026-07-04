28cb11b5dc6b8d1387b255787ef01d1d3633e0a7
chamath pathirana <chamathpathirana20@gmail.com>
2026-07-04 08:45:51 +0530

    jss

---
+++ js/app.js
@@ -0,0 +1,118 @@
+let name = "chamath";
+console.log(name);
+
+// let age = 34;
+// console.log(age);
+// let customers = false;
+// let customer = [];
+// let salary = 7500.0;
+
+
+//  let x = 10;
+// let y = 20;
+
+//  let z = x / y;
+
+//  console.log(z);
+
+// -------------------------------------------
+
+//  comparison operators
+
+// > | < | >= | <= | == | !=
+// >== | <== | === | !==
+
+
+//  let x = 11;
+
+//  let y = "11";
+
+//  console.log(x===y);
+
+
+// -------------------------------------------
+
+//  logical operators
+
+//  && | || | !
+
+//  if (!false) {
+//      console.log("true");
+//  } else {
+//      console.log("false");
+//  }
+
+// -------------------------------------------
+
+// if else statement
+
+//  let x = 10;
+//  let y = 20;
+
+//  if (x > y) {
+//      console.log("x is greater than y");
+//  } else if (x < y) {
+//      console.log("x is less than y");
+//  } else {
+//      console.log("x is equal to y");
+//  }
+
+// -------------------------------------------
+
+// switch statement
+
+// let day = 1;
+
+// switch (day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     default:
+//         console.log("Invalid day");
+// }
+
+// -------------------------------------------
+
+// for loop
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// -------------------------------------------
+
+// while loop
+
+// let i = 0;
+// while (i <= 10) {
+//     console.log(i);
+//     i++;
+// }
+
+// -------------------------------------------
+
+// do while loop
+
+// let i = 0;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 10);
+
+// let numbers = [1,2,3,4,5];
+//  for (let i = 0; i < numbers.length; i++){
+//     console.log(numbers[i]);
+//  }
+
+
+let numbers = [1,true,3,"saman",5,"ranil","sunil"];
+for (let i = 0; i <numbers.length; i++){
+     console.log(numbers[i]);
+}
+
